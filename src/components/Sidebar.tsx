1683ad12077f15f778e058375528712d47eb1e01
ZegarekPL <zegarekpl.gaming@gmail.com>
2025-02-17 18:10:33 +0100

    fix: #dev fix view in sidebar when there is no image

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar: React.FC<SidebarProps> = ({
 }) => {
     return (
         <div
-            className={`fixed top-0 left-0 h-full bg-background p-4 transform transition-transform duration-300 ease-in-out z-30 ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:static md:translate-x-0 md:w-1/6 overflow-y-auto`}
+            className={`fixed top-0 left-0 h-full w-full bg-background p-4 transform transition-transform duration-300 ease-in-out z-30 ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:static md:translate-x-0 md:w-1/6 overflow-y-auto`}
         >
             <button onClick={onClose} className="md:hidden text-white mb-4">
                 Zamknij
@@ -48,8 +48,8 @@ const Sidebar: React.FC<SidebarProps> = ({
                         >
                             <div className="flex w-full items-center">
                               <span className="font-bold text-md mr-2 shrink-0">{index + 1}</span>
-                                {slide.image && (
-                                    <div className="w-full">
+                                <div className="w-full">
+                                    {slide.image ? (
                                         <Image
                                             src={slide.image}
                                             alt={`Podgląd slajdu ${index + 1}`}
@@ -60,27 +60,37 @@ const Sidebar: React.FC<SidebarProps> = ({
                                             }`}
                                             onClick={() => onSelectSlide(index)}
                                         />
-                                    </div>
-                                )}
+                                    ) : (
+                                        <div
+                                            className={`h-40 w-full bg-lightbackground p-2 rounded cursor-pointer border-2 ${
+                                                selectedSlide === index ? 'border-blue-500' : 'border-transparent'
+                                            }`}
+                                            onClick={() => onSelectSlide(index)}
+                                        >
+                                            <p className="text-center text-gray-500">Brak obrazu</p>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
                             <div className="flex justify-between items-center mt-2 space-x-2">
-								<button
-                                	onClick={() => onAddSlideAt(index + 1)}
-                                	className="w-full p-2 bg-green-500 text-white rounded"
-                            	>
-                                	Dodaj slajd
-                            	</button>
-								{index > 0 && (
                                 <button
-                                    onClick={(e) => {
-                                        e.stopPropagation();
-                                        onRemoveSlide(index);
-                                    }}
-                                    disabled={slides.length === 1}
-									className="w-full p-2 bg-red-500 text-white rounded"
+                                    onClick={() => onAddSlideAt(index + 1)}
+                                    className="w-full p-2 bg-green-500 text-white rounded"
                                 >
-                                    Usuń slajd
-                                </button>)}
+                                    Dodaj slajd
+                                </button>
+                                {index > 0 && (
+                                    <button
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            onRemoveSlide(index);
+                                        }}
+                                        disabled={slides.length === 1}
+                                        className="w-full p-2 bg-red-500 text-white rounded"
+                                    >
+                                        Usuń slajd
+                                    </button>
+                                )}
                             </div>
                         </li>
                     </React.Fragment>
